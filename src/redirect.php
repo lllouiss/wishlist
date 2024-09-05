cd0dd7324b3672ecd6be5828060bd192db47413f
lw <lw@hli.ch>
2024-09-05 11:15:17 +0200

    WIP

---
+++ src/redirect.php
@@ -0,0 +1,15 @@
+<?php
+function redirect($url): void
+{
+    if (!headers_sent()) {
+        header('Location: ' . $url);
+    } else {
+        echo '<script type="text/javascript">';
+        echo 'window.location.href="' . $url . '";';
+        echo '</script>';
+        echo '<noscript>';
+        echo '<meta http-equiv="refresh" content="0;url=' . $url . '" />';
+        echo '</noscript>';
+    }
+}
+
